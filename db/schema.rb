79b60a4fa6bfe727cea43e9ea92b4301b3ce0f6e
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2019-11-26 21:32:55 +0200

    removed description from listing_photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_192547) do
+ActiveRecord::Schema.define(version: 2019_11_26_193010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_192547) do
 
   create_table "listing_photos", force: :cascade do |t|
     t.bigint "listing_id"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["listing_id"], name: "index_listing_photos_on_listing_id"
