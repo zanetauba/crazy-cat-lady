125c07fce573bc09ceb8c9ecbf3be6e507a46fdb
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2020-05-19 19:22:51 +0300

    signup form fixes - added a custom registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
- devise_for :users
+ devise_for :users, :controllers => { registrations: 'registrations' }
   get 'dashboard', to: 'users#dashboard'
   root to: 'pages#home'
   resources :listings
