394a29ce5ca69c15c9e0dfe526c6e775451f8ad7
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2020-05-24 22:20:45 +0300

    fixed booking-new validations

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 flatpickr("#range_start", {
+  minDate: "today",
   altInput: true,
   plugins: [new rangePlugin({ input: "#range_end"})]
 });
