fb1a6d9c1df0f6df3478c51fca9137c94f22687f
cd ~/code/$GITHUB_USERNAME/dotfiles <zaneta.uba@gmail.com>
2020-07-19 22:57:38 +0300

    booking show page frontend done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,14 @@ class UsersController < ApplicationController
     @user = current_user
     @requests = @user.requests
     @booking_info = @user.booking_info
-    @upcoming_bookings = @user.bookings.where(['ending_at >= ?', DateTime.now])
-    @expired_bookings = @user.bookings.where(['ending_at < ?', DateTime.now])
+    @ongoing_bookings = @user.bookings.where(['ending_at >= ? and starting_at <= ? and accepted = true',  DateTime.now, DateTime.now])
+    @upcoming_bookings = @user.bookings.where(['starting_at >= ?',  DateTime.now])
+    @expired_bookings = @user.bookings.where(['starting_at < ? and ending_at < ? and accepted = true', DateTime.now, DateTime.now]) + @user.bookings.where(['starting_at < ? and accepted = false', DateTime.now])
+
     @upcoming_hostings = @booking_info.where(['ending_at >= ?', DateTime.now])
     @expired_hostings = @booking_info.where(['ending_at < ?', DateTime.now])
-    @requests_to_accept = @requests.where(['ending_at > ? and starting_at >= ?', DateTime.now, DateTime.now])
-    @expired_requests = @requests.where(['ending_at <= ? and starting_at < ?', DateTime.now, DateTime.now])
+    @requests_to_accept = @requests.where(['starting_at >= ?',  DateTime.now])
+    @expired_requests = @requests.where(['starting_at < ?', DateTime.now])
 
     #@pet_id_booking = @booking_info.pet_id
     #@guest_pet = Pet.find(@pet_id_booking)
